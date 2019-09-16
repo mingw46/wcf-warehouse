d49c2c7c45aedc60a90beb26398914c0caead90d
Pawel Strupiechowski <Pawel.Strupiechowski@pl.edensprings.com>
2019-09-16 07:45:51 +0200

    Initial Commit

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public partial class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Column(TypeName="nvarchar(50)")]
+        [Required(ErrorMessage ="Required")]
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Email { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+        public Nullable<System.DateTime> LastLoginDate { get; set; }
+        public bool RembemberMe { get; set; }
+}
+}
